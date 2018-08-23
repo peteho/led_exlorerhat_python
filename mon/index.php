e253facc6189139a10a48c5c08a56347c7f8d451
peteho <peter@endres-hoppe.de>
2018-08-23 15:01:17 +0200

    index.php changed

--- mon/index.php
+++ mon/index.php
@@ -8,15 +8,13 @@
     </style>
   </head>
   <body>
-    <h1>raspi4 monitor</h1>
+    <h1>raspi2 monitor</h1>
     <pre><?php
         printf ( "<h2>%s</h2>\n", date ( DATE_RFC822 ) );
 
 	printf ( "<h2>" );
 	echo shell_exec('cat /tmp/workfile');
         printf ( "</h2>\n" );
-
-        //echo shell_exec('tail -n 20 /var/log/piupsmon.log');
     ?></pre>
   </body>
 </html>
